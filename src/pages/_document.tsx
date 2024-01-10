d5918ff3c72fb3d3bebea2236cc86c96f86990d8
mathf <Mathias>
2024-01-10 16:44:44 -0300

    shortcut icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,7 +28,7 @@ export default class MyDocument extends Document {
 
                <link rel="preconnect" href="https://fonts.gstatic.com" />
                <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
-               <link rel="shortcut icon" href="/static/img/logo.png" />
+               <link rel="shortcut icon" href="static/img/logo.png" />
                <React.Fragment>
                   {/* <!-- Facebook Pixel Code --> */}
                   <script dangerouslySetInnerHTML={{
