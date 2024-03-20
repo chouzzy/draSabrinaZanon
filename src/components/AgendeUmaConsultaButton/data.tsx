b46d0bac86ef2a092bc9afef2518f516928fcc11
chouzzy <mathfernando@hotmail.com>
2024-03-20 15:43:44 -0300

    zap novo 8888

--- src/components/AgendeUmaConsultaButton/data.tsx
+++ src/components/AgendeUmaConsultaButton/data.tsx
@@ -1,6 +1,6 @@
 
 
 export const AgendeUmaConsultaButtonData = {
-    href: "https://wa.me/554788015484?text=Olá, tudo bem? Acessei o site da Dra. Sabrina Zanon e gostaria de mais informações!",
+    href: "https://wa.me/5511988884611?text=Olá, tudo bem? Acessei o site da Dra. Sabrina Zanon e gostaria de mais informações!",
     instagram:"https://www.instagram.com/dra.sabrinamzanon/"
 }
